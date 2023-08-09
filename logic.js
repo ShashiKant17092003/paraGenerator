597d9dcf2e8e9769c028dd6291d9b9e630e60476
Shashi Kant <shashikantsln1793@gmail.com>
2023-08-09 15:25:19 +0530

    new

--- logic.js
+++ logic.js
@@ -1,38 +1,43 @@
 const lorem = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, fugiat.","Ducimus doloribus doloremque, asperiores consequatur nemo voluptatibus praesentium mollitia nulla!","Enim veritatis, blanditiis saepe tenetur dolore voluptatum libero nihil magnam.","Repudiandae possimus necessitatibus, neque laudantium sapiente odit amet consequatur eos.","Ea libero perspiciatis quo eligendi minima veniam. Quidem, dolores at!","Dolorem itaque deleniti necessitatibus facilis officia autem beatae, eum cumque?","Minus id ea saepe officiis voluptatem ipsa? Esse, quasi error!","Aspernatur eaque explicabo at officia esse nostrum a quisquam aut?","Voluptates similique quas qui fuga nisi vero, debitis id saepe.","Molestias ut, odio ab incidunt consectetur error non distinctio atque?"];
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
- }
- function Clear(){
+}
+
+
+function Clear() {
+    var head = document.getElementById('para').innerHTML = '';
     var clr = document.getElementById('box');
     clr.innerHTML = '';
- }
+}
 
- input.addEventListener("keypress", function(event) {
+
+const input = document.getElementById('num');
+input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-      event.preventDefault();
-      //document.getElementById("mybtn").click();
-      generate();
+        event.preventDefault();
+        generate();
     }
-  });
+});
 
- async function generate(){
-    const n = document.getElementById('num').value;
-    console.log(n);
+async function generate() {
+    const n = parseInt(document.getElementById('num').value);
     const div = document.getElementById('box');
     div.innerHTML = "";
-    for(var i = 0; i < n;i++){
-        for(var j = 0; j <10;j++){
-            var para = document.createElement('p');
-            for(var k = 0; k < 10; k++){
-                let x = Math.floor((Math.random() * 10) + 1);
-                var text = document.createTextNode(lorem[x]);
-
-                para.appendChild(text);
-            }
-            
-        }
-        await sleep(500);
+    for (var i = 0; i < n; i++) {
+        var head = document.getElementById('para');
+        head.innerHTML = (i + 1) + " Paragraphs generated";
+        var para = document.createElement('p');
         div.appendChild(para);
-        
+
+        for (var k = 0; k < 10; k++) {
+            let x = Math.floor(Math.random() * lorem.length);
+            for (var l = 0; l < lorem[x].length; l++) {
+                var text = document.createTextNode(lorem[x][l]);
+                para.appendChild(text);
+                await sleep(1);
+            }
+        }
+        para = document.createElement('p');
+        div.appendChild(para);
     }
 }
